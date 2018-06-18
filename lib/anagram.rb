b6e5b1a1c7a05971486a7376db6331a0ffd02a56
Brandon Green <brandon@brandonleeproductions.com>
2018-06-18 03:14:49 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,12 +3,14 @@ class Anagram
   attr_accessor :word
 
   @@words = []
+
   def initialize(word)
     @word = word
   end
 
   def match(word)
-
+    @word.each do |word| word.split("")
+    end
 
   end
 
